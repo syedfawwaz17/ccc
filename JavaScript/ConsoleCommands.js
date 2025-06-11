acc8a1603d710ac1c4257f6ea005d5b48073edac
syedfawwaz17 <syedfawwaz17@gmail.com>
2025-06-11 09:34:10 +0530

    First js

---
+++ JavaScript/ConsoleCommands.js
@@ -0,0 +1,7 @@
+alert("Your message here")
+// A built-in JavaScript function used to display a popup message box to the user.
+// It pauses the program until the user clicks “OK”.
+
+confirm("Are you sure?");
+// A built-in JavaScript function that shows a popup with "OK" and "Cancel" buttons.
+// It asks the user for confirmation (yes/no, true/false)
